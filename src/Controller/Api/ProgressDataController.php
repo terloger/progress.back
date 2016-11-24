2e30bd99ab4b7974fb3c9c180288b2d703190453
ter <i@ter.st>
2016-11-25 00:02:37 +0300

    custom data log

--- src/Controller/Api/ProgressDataController.php
+++ src/Controller/Api/ProgressDataController.php
@@ -56,4 +56,61 @@ class ProgressDataController extends DataAppController {
         $this->set('_serialize', ['success', 'data']);
     }
 
+	public function valuesLogHistory() {
+    	$this->loadModel('ValuesLog');
+
+		$units = $_REQUEST['units'];
+
+        $query = $this->ValuesLog->find()
+		->innerJoin(
+			['Days' => 'days'],
+			[
+				'ValuesLog.day_id = Days.id',
+				'Days.user_id' => $this->Auth->user('id')
+			]
+		)
+        ->where([
+			'ValuesLog.unit_id IN' => $units
+		])
+		->group(['ValuesLog.day_id', 'ValuesLog.unit_id'])
+		->order(['Days.date' => 'DESC'])
+		->limit(100);
+
+		$unit = [];
+		foreach ($units as $key => $value) {
+			$unit[$key] = $query->newExpr()->addCase([$query->newExpr()->add('ValuesLog.unit_id = ' . $value)], [ new \Cake\Database\Expression\IdentifierExpression('ValuesLog.value'), 0], ['integer','integer']);
+		}
+
+		$selectParams = ['date' => 'Days.date'];
+		
+		foreach ($units as $key => $value) {
+			$selectParams['unit_' . $value] = $unit[$key];
+		}
+
+		$sub = $query->select($selectParams);
+
+		$lastSelectParams = [
+			'id' => $query->func()->concat([
+					'(UNIX_TIMESTAMP(sub.date))' => 'literal'
+				]),
+			'date' => 'sub.date'
+		];
+
+		foreach ($units as $key => $value) {
+			$lastSelectParams['unit_' . $value] = $query->func()->max('sub.unit_' . $value);
+		}
+
+		$data = $this->ValuesLog->find()
+		->from(['sub' => $sub])
+		->group(['sub.date'])
+		->order(['sub.date' => 'DESC'])
+		->limit(30)
+		->select($lastSelectParams);
+
+		$success = true;
+		//debug($data);
+
+        $this->set(compact('data', 'success'));
+        $this->set('_serialize', ['success', 'data']);
+    }
 }
